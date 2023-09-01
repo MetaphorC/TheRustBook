344d6050d84b491b96e5f517aa1c23783b9397fe
MetaphorC <ypstomar57@gmail.com>
2023-09-01 23:49:49 +0530

    Chapter 3.1 and Chapter 3.2

--- common_concepts/src/main.rs
+++ common_concepts/src/main.rs
@@ -1,5 +1,48 @@
-fn main() {
-    let mut spaces = "   ";
-    spaces = spaces.len();
-    println!("{space}");
-}
+//
+// // 3.1 - Variables and Mutability
+//
+// // fn main() {
+//    // let mut spaces = "   ";
+//    // spaces = spaces.len();
+//    // println!("{space}");
+// // }
+//
+//
+//
+// 3.2 - Data Types
+//
+    // Scaler Types - int, float, bool, and char
+        // Integer
+            // u/i8, u/i16, u/i32/, u/i64, u/i128, and u/isize (depends of architecture)
+            // let guess: u32 = "42".parse().expect("Not a number"); k
+        // Floating point number 
+            // f32 (single precision), and f64 (double precision);
+            // fn main () {
+                // let x = 2.0; // f64
+                // let y: f32 = 3.0; // f32
+            // }
+        // Boolean
+            // True, and False
+            // fn main() {
+                // let t = true;
+                // let f: bool = false; // with explicit type annotation
+            // }
+        // Character 
+            // Most primitive alphabetic type 
+            // fn main() {
+                // let c = 'z';
+                // let z: char = 'Z'; // with explicit type annotation
+                // let heart_eyed_cat = '😻';
+            // }
+    // Compound Types - tuples and arrays
+        // Tuples 
+            // fn main() {
+                // let tup: (i32, f64, u8) = (500, 6.4, 3);
+            // }
+        // Arrays
+            // fn main() 
+                // let a = [q, r, s, t];
+                // let b: [i32; 5] = [1, 3, 4, 6, 9]; // Explicitly annotating type and length
+                // let c = [3; 5] // Array contains 3 five times
+            // }
+            
