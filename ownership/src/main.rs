65b1d89b6fc448a41487c3faa3e1f3ece07d6dcc
MetaphorC <ypstomar57@gmail.com>
2025-03-16 20:55:05 +0530

    updating remote with local

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,17 @@
-fn main() {
-    let s = "hello";  // s is valid from here
+//fn main() {
+//    let s = "hello";  // s is valid from here
+//
+//    // do stuff with s
+//}   // scope for s ended
+    //
+    //
+    // 1. each value has an owner
+    // 2. there can be only one owner for a value at a time
+    // 3. when the owner goes out of scope, the value will be dropped
 
-    // do stuff with s
-}   // scope for s ended
+
+fn main() {
+    let mut s = String::from("Holy ");
+    s.push_str("MOLY!"); // push_str() appends the string literal
+    println!("{s}") // this will print Holy Moly
+}
